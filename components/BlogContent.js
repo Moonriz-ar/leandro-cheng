7ed889a1172805bfe77e5f99af4609c7d9d96524
Andrea Lin <andre93.lin@gmail.com>
2022-01-21 13:20:40 -0300

    fixed error en BlogContent component, added check if there is tag in the content object

--- components/BlogContent.js
+++ components/BlogContent.js
@@ -1,15 +1,22 @@
 import Link from "next/link";
 
-const BlogContent = ({ content }) => {
+const renderTag = (content) => {
   return (
-    <article className="bg-gray-50 p-5 rounded-lg -mt-8 z-10 mx-4 drop-shadow-lg prose">
-      <h1 className="text-center">{content.title}</h1>
+    <>
       {content.tags.map((tag) => (
         <Link key={tag.id} href={`/tag/${tag.slug}`}>
           <a>#{tag.name}</a>
         </Link>
       ))}
+    </>
+  );
+};
 
+const BlogContent = ({ content }) => {
+  return (
+    <article className="z-10 p-5 mx-4 -mt-8 prose rounded-lg bg-gray-50 drop-shadow-lg">
+      <h1 className="text-center">{content.title}</h1>
+      {content.tags ? renderTag(content) : null}
       <div dangerouslySetInnerHTML={{ __html: content.html }}></div>
     </article>
   );
